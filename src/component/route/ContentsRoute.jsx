e9742a7488f811f9be4cf2afbea3efee4d1e0807
yeominsoo <minsoo6110@solomontech.net>
2021-03-18 18:10:54 +0900

    chat comp

--- src/component/route/ContentsRoute.jsx
+++ src/component/route/ContentsRoute.jsx
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 import Home from 'component/Home';
 import Line from 'component/chart/line/Line';
 import Bar from 'component/chart/bar/Bar';
+import Chat from 'component/chat/Chat';
 
 const AppRouter = () => {
   return (
@@ -10,6 +11,7 @@ const AppRouter = () => {
       <Route exact path="/" component={Home} />
       <Route path="/line" component={Line} />
       <Route path="/bar" component={Bar} />
+      <Route path="/chat" component={Chat} />
     </Switch>
   );
 };
