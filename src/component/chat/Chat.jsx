014ca11c865a15fac3c0c42d803646aa47429333
yeominsoo <minsoo6110@solomontech.net>
2021-03-23 16:52:05 +0900

    font css update

--- src/component/chat/Chat.jsx
+++ src/component/chat/Chat.jsx
@@ -1,16 +1,30 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState,memo } from "react";
 import "./Chat.css";
 //material ui
-import StarOutlineIcon from "@material-ui/icons/StarOutline";
 import db from "../../firebase";
 import Message from "./Message";
 import ChatInput from "./ChatInput";
 
-const Chat = () => {
-  const [roomId, setRoomId] = useState('DVWjjfbmPMGni08saFJA');
+const fn_ranColorSet = () => {
+  let colorArray = [];
+  for(let i=0;i<20;i++){
+    colorArray.push(`#${Math.round(Math.random() * 0xFFFFFF).toString(16)}`);
+  }
+  return colorArray;
+}
+
+const Chat = memo(() => {
+  const [roomId, setRoomId] = useState(null);
   const [roomDetails, setRoomDetails] = useState(null);
   const [roomMessages, setRoomMessages] = useState([]);
   const [channels, setChannels] = useState([]);
+  const [ranColor, setRanColor] = useState(fn_ranColorSet);
+  let ranIdx = 0;
+
+  const fn_getRandomColor = (length, curIdx) => {
+    (length-1) === curIdx? ranIdx = 0 : ranIdx++;
+    return ranColor[ranIdx];
+  }
   
   useEffect(() => {
     db.collection("rooms").onSnapshot((snapshot) =>
@@ -25,20 +39,21 @@ const Chat = () => {
       db.collection("rooms")
         .doc(roomId)
         .onSnapshot((snapshot) => setRoomDetails(snapshot.data()));
-    }
-    db.collection("rooms")
+    
+      db.collection("rooms")
       .doc(roomId)
       .collection("messages")
       .orderBy("timestamp", "asc")
       .onSnapshot((snapshot) =>
         setRoomMessages(snapshot.docs.map((doc) => doc.data()))
       );
+    }
   }, [roomId]);
 
   const selectChannel = (e) =>{
     console.log(e.target);
-    console.log(e.target.id);
-    setRoomId(e.target.id);
+    console.log(e.target.attributes.value2.value);
+    setRoomId(e.target.attributes.value2.value);
   }
   const addChannel = ()=>{
     const channelName = prompt('Please enter the channel name');
@@ -47,14 +62,15 @@ const Chat = () => {
           name:channelName,
       })
     }
+    setRanColor(fn_ranColorSet);
   };
   const removeChannel = (e) => {
-    
     let channelId = e.target.attributes.value2.value;
     let msg = "정말 삭제 하시겠습니까?\n삭제하시면 모든 대화 내용이 삭제됩니다.";
     if(window.confirm(msg)){
       db.collection("rooms").doc(channelId).delete().then(() => {
         alert("채널이 [삭제] 됐습니다.");
+        setRanColor(fn_ranColorSet);
       }).catch((error) => {
         alert(`[code : ${error}]삭제 중 에러가 발생했습니다.`);
       });
@@ -63,33 +79,35 @@ const Chat = () => {
   return (
     <>
       <div className="chat-container">
-        <div className="channel-wrap"  >
+        <div className="channel-wrap">
           <div className="channel-btn-zone">
-            <button type="button" onClick={addChannel} className="channel-add-btn">Add Channel</button>
+            <button type="button" onClick={addChannel} className="channel-add-btn"><span>+</span></button>
           </div>
           <ul className="channel-list">
-            {channels.map((channel) => (
+            {channels.map((channel,idx) => (
               <li>
-                <span className="channel-thumb">{channel.name.substring(0,1)}</span>
-                <div onClick={selectChannel} id={channel.id} className="channel-div">{channel.name}
+                <div onClick={selectChannel} value2={channel.id} className="channel-list-div">
+                  <span className="channel-list-thumb" value2={channel.id} style={{
+                    borderColor:fn_getRandomColor(channels.length,idx)
+                  }}>{channel.name.substring(0,1)}</span>
+                  <span className="channel-list-name" value2={channel.id} >{channel.name}</span>
                 </div>
-                <span onClick={removeChannel} value2={channel.id} className="channel-span">삭제</span>
+                <div className="channel-del-btn" value2={channel.id}>
+                  <span onClick={removeChannel} value2={channel.id} >삭제</span>
+                </div>
               </li> 
             ))}
           </ul>
         </div>
         <div className="chat-wrap">
-          {/* <h2> You are in the {roomId} </h2> */}
           <div className="chat__header">
             <div className="chat__headerLeft">
               <span className="chat__channelName">
                 <strong>{roomDetails?.name}</strong>
-                {/* <StarOutlineIcon /> */}
               </span>
             </div>
           </div>
           <div className="chat__messages">
-              {/* <Message .../> */}
               {roomMessages.map(({message , timestamp, user, userImage})=>(
                   <Message
                   message={message}
@@ -105,6 +123,6 @@ const Chat = () => {
       </div>
     </>
   );
-};
+});
 
 export default Chat;
