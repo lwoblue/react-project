bae585c8b765d4eba3405a30a0292d6748c062b3
ggovajy <ggova@naver.com>
2021-04-07 12:48:55 +0900

    images

--- server/database/db_mysql.js
+++ server/database/db_mysql.js
@@ -1,10 +1,10 @@
 var mysql = require("mysql2");
 
 const con = mysql.createConnection({
-    host: "192.168.0.18",
-    user: "solomon",
-    password: "admin",
-    database: "react", // database name
+  host: "192.168.0.111",
+  user: "solomon",
+  password: "admin",
+  database: "react", // database name
 });
 
 module.exports = con;
