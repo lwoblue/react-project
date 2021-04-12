9908b8462cf36d559fb04b1e5b8022b9f7096ca0
lwo_blue <lwo_blue@naver.com>
2021-04-12 13:16:12 +0900

    commit

--- server/database/db_mysql.js
+++ server/database/db_mysql.js
@@ -1,7 +1,7 @@
 var mysql = require('mysql2');
 
 const con = mysql.createConnection({
-  host: '192.168.0.111',
+  host: 'localhost',
   user: 'solomon',
   password: 'admin',
   database: 'react', // database name
